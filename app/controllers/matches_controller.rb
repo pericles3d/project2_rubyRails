dad73ff7c9b736110c3b0687bf4b2cafa6af1371
pericles <pericles3d@gmail.com>
2015-07-28 19:06:53 -0700

    create all matches page

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,10 +1,27 @@
 class MatchesController < ApplicationController
   def index
+    @match = Match.all
   end
 
   def new
+    @match = Match.new
   end
 
   def create
+    @match = Match.new(secure_params)
+    if @match.save
+      redirect_to users_path
+    else
+      render 'new'
+    end
   end
+
+  def show
+  end
+
+private
+  def secure_params
+    params.require(:match).permit(:score1, :score2)
+  end
+
 end
