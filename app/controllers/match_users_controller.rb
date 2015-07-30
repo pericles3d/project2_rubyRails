6c1059122ea8c5161fad79b019b21336d4ba509e
pericles <pericles3d@gmail.com>
2015-07-29 19:33:15 -0700

    user poll submission working and saving

--- app/controllers/match_users_controller.rb
+++ app/controllers/match_users_controller.rb
@@ -10,14 +10,20 @@ class MatchUsersController < ApplicationController
   end
 
   def update
-    params[:match].each do |k, v|
-      # logger.debug "match #{k} result #{v}"
-      v.each do |a, b|
-        logger.debug "score #{b}"
+    @user = current_user
+    if @user
+      @userMatches = MatchUser.all.where(:user_id => @user.id)
+      @userMatches.each do |m|
+        # logger.debug "result: #{params[:match]["#{m.match.id}"]["#{m.match.teams.first.id}"]}"
+        m.pScore1 = params[:match]["#{m.match.id}"]["#{m.match.teams.first.id}"]
+        m.pScore2 = params[:match]["#{m.match.id}"]["#{m.match.teams.last.id}"]
+        m.save
       end
+    else
+      flash[:notice] = "Must be logged in to submit ticket"
+      redirect_to root_path
     end
-    #use the params to update the match scores
-    redirect_to user_path
+    redirect_to user_path @user
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,16 @@ class UsersController < ApplicationController
     end
   end
 
+  def show
+    @user = current_user
+    if @user
+      @userMatches = MatchUser.all.where(:user_id => @user.id)
+    else
+      flash[:notice] = "Must login to view your matches"
+      redirect_to root_path
+    end
+  end
+
 
   private
 
