693d851ac8e96e565e7a9d117a371a46b5572ccb
pericles <pericles3d@gmail.com>
2015-07-29 20:18:24 -0700

    adjust routes in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,23 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @user = current_user
+    if @user
+      @userMatches = MatchUser.all.where(:user_id => @user.id)
+      redirect_to user_path @user
+    end
   end
 
   def new
     @user = User.new
+
   end
 
   def create
     @user = User.new(secure_params)
     @user.matches << Match.all
     if @user.save
+      session[:user_id] = @user.id
       redirect_to match_users_path
     else
       render 'new'
